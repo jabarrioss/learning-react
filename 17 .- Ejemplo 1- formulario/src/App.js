dcbc7d0d6af33194f5723d30467312106c83e92e
Jhonny Armando Barrios Sandrea <jbarriossandrea@gmail.com>
2021-08-08 22:44:40 -0400

    componentDidUpdate Example

--- 17 .- Ejemplo 1- formulario/src/App.js
+++ 17 .- Ejemplo 1- formulario/src/App.js
@@ -4,14 +4,34 @@ class App extends Component{
   constructor(props){
     super(props);
     console.log("Constructor");
+
+    this.state = {
+      name: ""
+    };
   }
 
   componentDidMount(){
     console.log("Component did mount");
   }
 
+
+  componentDidUpdate(){
+    console.log("Component did update");
+    console.log(this.state.name);
+  }
+
   render(){
-    return ( <div>Hola</div> );
+    return ( <div>
+      {/* 
+        Here we use destructuring to extract the
+        value prop from the event object
+        event.target.value = {target: { value }}
+       */}
+
+       {/* Also since we are using a class component for this example we use the this.setState method to update the current state of the component */}
+       {/* Note from the teacher: this way of updating components is only recommended when you are perfoming just 1 action in the event handler */}
+      <input type="text" value={this.state.name} onChange={({target : { value }}) => this.setState({name:value})} />
+    </div> );
   }
 
 }
